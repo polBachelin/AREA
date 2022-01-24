0f50f3d7952ee525c2c81f25a92fa8bd55dec129
Killian <killian.vallette@epitech.eu>
2022-01-24 15:56:10 +0100

    fix(server): fix main.ts and remove the duplication

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards, Post, Request} from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { Public } from './custom.decorators';
 
 @Controller('/')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private authService : AuthService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Public()
+  @UseGuards(LocalAuthGuard)
+  @Post('/auth/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @Get('/user/profile')
+  getCurrentUser(@Request() req) {
+    return req.user;
   }
 }
