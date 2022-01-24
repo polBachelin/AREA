fe25553ce5587b2d72d2364bbe3690a5ecfd2fcf
Pol Bachelin <pol.bachelin@epitech.eu>
2022-01-24 15:35:47 +0100

    feat(auth): fixed auth module and service after merge

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { UsersModule } from '../users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtSecret } from './constant';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  providers: [AuthService]
+  imports: [UsersModule,
+  PassportModule,
+  JwtModule.register({
+    secret: jwtSecret.secret,
+    signOptions: { expiresIn: '3000000000s'},
+  }),
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  exports: [AuthService]
 })
+
 export class AuthModule {}
