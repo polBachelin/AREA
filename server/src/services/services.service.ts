05385b9fc4d9a2900060fbfb11d06e9ba6d34741
Pol Bachelin <pol.bachelin@epitech.eu>
2022-01-25 09:28:20 +0100

    feat(services.service): moved allServices variable to file

--- server/src/services/services.service.ts
+++ server/src/services/services.service.ts
@@ -1,12 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { IService } from './interfaces/service.interface';
-import { Notion } from './entities/Notion/Notion.class';
-import { Discord } from './entities/Discord/Discord.class';
-
-let allServices: IService[] = [];
-allServices.push(Notion);
-allServices.push(Discord);
-
+import { allServices } from './allServices';
 
 @Injectable()
 export class ServicesService {
