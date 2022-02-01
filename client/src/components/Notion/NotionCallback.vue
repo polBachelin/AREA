59d4d55ab0cde569d6db4095d2f4a4407e30fbf1
Pol Bachelin <pol.bachelin@epitech.eu>
2022-02-01 17:17:08 +0100

    feat(notion.js): removed callback to fix invalid grant in authorize

--- client/src/components/Notion/NotionCallback.vue
+++ client/src/components/Notion/NotionCallback.vue
@@ -14,8 +14,7 @@ window.onload = function() {
 	console.log("got it")
 	const urlParams = new URLSearchParams(window.location.search);
 	const code = urlParams.get('code');
-	sendCode(code);
-	
+	sendCode(code);	
 }
 
 
