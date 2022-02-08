2b2ea519217739393e7ebc168f20639dbb616262
Pol Bachelin <pol.bachelin@epitech.eu>
2022-02-08 20:51:45 +0100

    feat(area.service): added data properties to reaction

--- server/src/area/area.service.ts
+++ server/src/area/area.service.ts
@@ -2,7 +2,7 @@ import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { allServices, ActionsFactory, actionKeys, ReactionsFactory, reactionKeys } from "src/services/allServices";
 import { Area } from "src/area/area.class"
 import { ATrigger } from "src/interfaces/trigger.interface";
-import { ATask } from "src/interfaces/task.interface";
+import { ATask, properties } from "src/interfaces/task.interface";
 import { InjectModel } from "@nestjs/mongoose";
 import { IArea } from "src/models/Area";
 import { Model } from 'mongoose';
@@ -16,12 +16,12 @@ export class AreaService {
 		@InjectModel('Area') private areaModel: Model<IArea>,
 	) {}
 
-	public async createArea(userEmail: string, areaName: string, actionName: actionKeys, reactionName: reactionKeys) {
+	public async createArea(userEmail: string, areaName: string, actionName: actionKeys, reactionName: reactionKeys, data: properties) {
 		if (await this.areaModel.findOne({areaName})) {
 			throw new HttpException('area already exists', HttpStatus.BAD_REQUEST);
 		}
 		let areaAction = ActionsFactory.buildTask(actionName, actionName, 0) as ATrigger;
-		let areaReaction = ReactionsFactory.buildTask(reactionName as reactionKeys, 0, reactionName, undefined) as ATask;
+		let areaReaction = ReactionsFactory.buildTask(reactionName as reactionKeys, 0, reactionName, data) as ATask;
 		const area: Area = new Area({
 			name: areaName,
 			action: areaAction,
