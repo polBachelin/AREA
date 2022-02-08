3e2d02ef74d6a9b6ffdea2ba4b371186a0fa0a91
Pol Bachelin <pol.bachelin@epitech.eu>
2022-02-08 19:51:53 +0100

    feat(Area): added Area schema for DB and deleted useless model files

--- server/src/area/area.service.ts
+++ server/src/area/area.service.ts
@@ -1,14 +1,23 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { allServices, ActionsFactory, actionKeys, ReactionsFactory, reactionKeys } from "src/services/allServices";
 import { Area } from "src/area/area.class"
 import { ATrigger } from "src/interfaces/trigger.interface";
 import { ATask } from "src/interfaces/task.interface";
-
+import { InjectModel } from "@nestjs/mongoose";
+import { IArea } from "src/models/Area";
+import { Model } from 'mongoose';
 
 @Injectable()
 export class AreaService {
 
-	public createArea(areaName: string, actionName: actionKeys, reactionName: reactionKeys): Area {
+	constructor(
+		@InjectModel('Area') private areaModel: Model<IArea>,
+	) {}
+
+	public async createArea(areaName: string, actionName: actionKeys, reactionName: reactionKeys) {
+		if (await this.areaModel.findOne({areaName})) {
+			throw new HttpException('area already exists', HttpStatus.BAD_REQUEST);
+		}
 		let areaAction = ActionsFactory.buildTask(actionName, "Add to database", 0) as ATrigger;
 		let areaReaction = ReactionsFactory.buildTask(reactionName) as ATask;
 		const area: Area = new Area({
@@ -16,6 +25,12 @@ export class AreaService {
 			action: areaAction,
 			reaction: areaReaction,
 		});
+		const newArea = new this.areaModel();
+		await newArea.save();
 		return area;
+	}
+
+	public enableAnArea(areaName: string) {
+
 	}
 }
