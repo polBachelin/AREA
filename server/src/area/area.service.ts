0aa595ff8f08d719a2f554aa6be74825ef6aa18b
Pol Bachelin <pol.bachelin@epitech.eu>
2022-02-08 19:37:51 +0100

    feat(area): added area class with disable, enable and launch functions

--- server/src/area/area.service.ts
+++ server/src/area/area.service.ts
@@ -1,6 +1,21 @@
-import { Inject, Injectable } from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
+import { allServices, ActionsFactory, actionKeys, ReactionsFactory, reactionKeys } from "src/services/allServices";
+import { Area } from "src/area/area.class"
+import { ATrigger } from "src/interfaces/trigger.interface";
+import { ATask } from "src/interfaces/task.interface";
+
 
 @Injectable()
 export class AreaService {
 
+	public createArea(areaName: string, actionName: actionKeys, reactionName: reactionKeys): Area {
+		let areaAction = ActionsFactory.buildTask(actionName, "Add to database", 0) as ATrigger;
+		let areaReaction = ReactionsFactory.buildTask(reactionName) as ATask;
+		const area: Area = new Area({
+			name: areaName,
+			action: areaAction,
+			reaction: areaReaction,
+		});
+		return area;
+	}
 }
