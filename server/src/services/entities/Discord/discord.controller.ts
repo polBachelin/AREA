a4a84270f90b1c17dae6b88ff49398ab43fc3606
Killian <killian.vallette@epitech.eu>
2022-02-15 15:57:26 +0100

    feat(discord): test discord token on db

--- server/src/services/entities/Discord/discord.controller.ts
+++ server/src/services/entities/Discord/discord.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Redirect, Query } from "@nestjs/common";
+import { Controller, Get, Redirect, Query, Logger } from "@nestjs/common";
 import { ApiOperation, ApiTags } from "@nestjs/swagger";
 import { AuthService } from "src/auth/auth.service";
 import { RegisterDTO } from "src/users/register.dto";
@@ -18,22 +18,31 @@ export class DiscordController {
 	async discordCallback(@Query() query) {
 		let email: string = null;
 		let discordToken = null;
-		this.discordService.authorize(query.code).then((res) => {
+		await this.discordService.authorize(query.code).then((res) => {
 			discordToken = res;
-			this.discordService.getUserEmail(discordToken.access_token).then((res) => {
-				email = res;
-			}).catch(err => {
-				console.log(err);
-			})
+			// Logger.log("2");
+			// Logger.log(discordToken)
+			// this.discordService.getUserEmail(discordToken.access_token).then((res) => {
+			// 	email = res;
+			// 	Logger.log(res)
+			// }).catch(err => {
+			// 	console.log(err);
+			// })
 		})
+		if (discordToken) {
+			email = await this.discordService.getUserEmail(discordToken.access_token)
+		}
 		if (email) {
 			let user = await this.userService.findOne(email);
+			Logger.log("4");
 			if (!user) {
 				let RegisterDTO: RegisterDTO;
 				RegisterDTO = {email:email, password:''};
 				user = this.userService.createUser(RegisterDTO);
+				Logger.log("5");
 			}
-			this.discordService.setDiscordToken(user, discordToken);
+			this.discordService.setDiscordToken(email, discordToken);
+			Logger.log("6");
 			const token = await this.authService.signUser(user);
 			return { url: 'http://localhost:8080/home?email=' + email + '&token=' + token.access_token};
 		}
