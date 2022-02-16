84e32326cd5c274c0535341072f1bcb2591a2c09
zac <borzach>
2022-02-16 16:25:32 +0100

    fix(test): add test dir

---
+++ test/testLogin.py
@@ -0,0 +1,24 @@
+import json
+import sys
+
+print('Testing LOGIN ...')
+f = open("test.txt","r")
+lines = f.readlines()
+if len(lines) == 0:
+    print('Fail to connect')
+    sys.exit()
+
+for x in lines :
+    if x[0] == '{':
+        jsonLine = x
+    if x.rfind('HTTP') != -1:
+        errorCode = x.split(' ')[1]
+parsed = json.loads(jsonLine)
+if errorCode.rfind('201') == -1:
+    print("Bad Request , error code : ", errorCode)
+    sys.exit()
+if not parsed["token"]["access_token"]:
+    print('Fail , no acces token')
+    sys.exit()
+
+print('Succes')
