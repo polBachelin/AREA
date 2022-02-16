689f53d67e8d977aa363db1d2a3c3643e8660983
Pol Bachelin <pol.bachelin@epitech.eu>
2022-02-16 15:24:50 +0100

    fix(unitTests): removed sudo from unitTests

--- server/src/services/entities/Notion/actions/AddToDB.ts
+++ server/src/services/entities/Notion/actions/AddToDB.ts
@@ -10,7 +10,7 @@ export class AddToDB extends ATrigger {
 		this.intervalObj = setInterval(async () => {
 			this.lastUpdateCheck = new Date()
 			const db_id = this.data.get('database_id') || ''
-			const access_token = this.data.get('access_token')
+			const access_token = this.data.get('access_token') || ''
 			let db = await notionClient.databases.retrieve({auth: access_token, database_id: db_id});
 			let newDate = new Date(db.last_edited_time)
 			if (newDate > this.lastUpdateCheck) {
