84a49ea3fa29d1cafcb20d93b6ef1ba18511d296
Pol Bachelin <pol.bachelin@epitech.eu>
2022-02-21 18:40:46 +0100

    feat(SendMessage): sending message to guild channel reaction

--- server/src/services/entities/Discord/reactions/SendMessage.ts
+++ server/src/services/entities/Discord/reactions/SendMessage.ts
@@ -1,21 +1,24 @@
 import { ATask, properties } from "../../../../interfaces/task.interface";
-import { Client } from 'discord.js'
+import { Client, GuildChannel, TextChannel } from 'discord.js'
 
 export class SendMessage extends ATask {
 
 	client = new Client({intents: "GUILD_MESSAGES"});
 
-	run(user: any): Promise<any> {
+	async run(user: any): Promise<any> {
 		if (!this.client.token) this.client.login(user.discord.access_token);
-		const id = this.data.get('user_id');
+		const id = this.data.get('guild_id');
 		const content = this.data.get('message_content');
-
-		//this.client.on('send')
-		// this.client.users.fetch(id).then((res) => {
-		// 	console.log(res)
-		// }).catch((err) => {
-		// 	console.log(err)
-		// })
+        if (!id || !content) throw 'Invalid Data'
+		let channel = undefined;
+		
+		this.client.on('ready', () => {
+			channel = this.client.channels.fetch(id);
+			console.log(channel);
+		})
+        if (!channel) throw 'Invalid Channel'
+        const tc = channel as TextChannel
+        await tc.send(content)
 		console.log("sending message discord task")
 		return undefined;
 	}
