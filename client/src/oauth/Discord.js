3110f563e535ef5c73ac51ec648baeea721a9712
Pol Bachelin <pol.bachelin@epitech.eu>
2022-02-22 09:54:33 +0100

    feat(discord.js): added authorization to discord url

--- client/src/oauth/Discord.js
+++ client/src/oauth/Discord.js
@@ -1,7 +1,7 @@
 export const discordUrl = "https://discord.com/api/oauth2/authorize?"
 			+ "response_type=code"
 			+ "&client_id=286959581488480267"
-			+ "&scope=identify%20email"
+			+ "&scope=identify%20email%20applications.commands%20activities.write"
 			+ "&state="
 			+ "&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fdiscord%2Fauth"
 			+ "&prompt=consent"
