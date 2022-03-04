107c77c0735cc3afaacd35b340545036d7bea211
Killian <killian.vallette@epitech.eu>
2022-03-04 15:59:30 +0100

    fix(weather): fix meteo request

--- server/src/services/entities/Weather/actions/GetCityWeather.ts
+++ server/src/services/entities/Weather/actions/GetCityWeather.ts
@@ -4,25 +4,27 @@ import axios, {AxiosRequestConfig} from 'axios';
 import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
 import { properties } from "src/interfaces/task.interface";
 
-const WEATHER_URL = "api.openweathermap.org/data/2.5/weather?q="
-const API_KEY = "&appid=899c451b2f906345aeab6458c2febc9d"
+const WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?units=metric&q="
+const API_KEY = "&appid=b362492d7885049baca48386d84b710d"
 const WEATHER_REFRESH_RATE = 10000
 
 export class WeatherChange extends ATrigger {
 
-    public async getWeather(city_name: String) {
+    public getWeather(city_name: String) {
         const api_url = WEATHER_URL + city_name + API_KEY;
-        let res = await axios.get(api_url);
-        if (!res) {
-            return new HttpException('Invalid autologin', HttpStatus.BAD_REQUEST);
-        }
-        return res.data.weather[0].main;
+        let res = axios.get(api_url).then((res) => {
+            if (!res) {
+                return new HttpException('Invalid autologin', HttpStatus.BAD_REQUEST);
+            }
+            return res.data.weather[0].main;
+        })
     }
 
     public setup(callback: () => Promise<void>, user: IUser): void {
-        const city_name = this.data.get('city_name');
+        const city_name = this.data.get('city');
         const api_url = WEATHER_URL + city_name + API_KEY;
         let old_weather = this.getWeather(city_name);
+        console.log("Weater : "+old_weather);
         this.setChecking();
         this.intervalObj = setInterval(async () => {
             Logger.log('Wheather is checking');
@@ -34,6 +36,6 @@ export class WeatherChange extends ATrigger {
                 this.lastExec = new Date();
                 old_weather = new_weather;
             }
-        })
+        }, WEATHER_REFRESH_RATE)
     }
 }
