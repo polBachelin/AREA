dd4dbd4d3a0e90409807330f2e6b41961f29ea12
jules-omar.marlin@epitech.eu <jules-omar.marlin@epitech.eu@epitech.eu>
2022-03-06 17:37:06 +0100

    feat(apk): add a /client.apk to dl the apk

---
+++ client/src/components/Apk.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container class="justify-center text-center">
+    <v-divider class="mt-10"></v-divider>
+    <v-row class="justify-center text-center mt-10">
+      <a id="ApkDL" href="/files/app-release.apk" download>
+        DOWNLOAD APK
+      </a>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Apk.vue",
+
+  mounted() {
+    document.getElementById("ApkDL").click()
+  },
+}
+</script>
